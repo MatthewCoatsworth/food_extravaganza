6ba038b22143d381d70005045ea1274a87ba1de3
MatthewCoatsworth <80717808+MatthewCoatsworth@users.noreply.github.com>
2022-04-23 13:34:50 +1200

    Add files via upload

---
+++ mobile_app/mobile_app(backup)/main.js
@@ -0,0 +1,100 @@
+/*
+var carousel = document.querySelector('.carousel');
+var cellCount = 9;
+var selectedIndex = 0;
+
+function rotateCarousel() {
+  var angle = selectedIndex / cellCount * -360;
+  carousel.style.transform = 'translateZ(-288px) rotateY(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
+*/
+
+
+
+
+
+
+
+
+
+var slide_index = 1;
+displaySlides(slide_index);
+
+function nextSlide(n) {
+    displaySlides(slide_index += n);
+}
+function currentSlide(n) {
+    displaySlides(slide_index = n);
+}
+
+function displaySlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("showSlide");
+    if (n > slides.length) { slide_index = 1 }
+    if (n < 1) { slide_index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slide_index - 1].style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+*/
